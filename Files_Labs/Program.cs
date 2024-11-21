9e75c896b835f3b69ada80cdb72f9e205b64c454
AnnaParysheva <parysevaanna36@gmail.com>
2024-11-21 20:51:09 +0300

    Добавьте файлы проекта.

---
+++ Files_Labs/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Files_Labs
+{
+    internal class Program
+    {
+        static double Calculation_Array(out double sr_arif, ref long proizv, params int[] array)
+        {
+            double sum = 0;
+            proizv = 1;
+            foreach (int number in array)
+            {
+                sum += number;
+                proizv *= number;
+            }
+            sr_arif = sum / array.Length;
+            return sum;
+        }
+        static void Draw_Digit(int digit)
+        {
+            switch (digit)
+            {
+                case 0:
+                    Console.WriteLine(" #### \n#    #\n#    #\n#    #\n #### ");
+                    break;
+                case 1:
+                    Console.WriteLine("   # \n   # \n   # \n   # \n   # ");
+                    break;
+                case 2:
+                    Console.WriteLine(" #### \n     #\n #### \n#     \n #### ");
+                    break;
+                case 3:
+                    Console.WriteLine(" #### \n     #\n #### \n     #\n #### ");
+                    break;
+                case 4:
+                    Console.WriteLine("#   # \n#   # \n #### \n    # \n    # ");
+                    break;
+                case 5:
+                    Console.WriteLine(" #### \n#     \n #### \n     #\n #### ");
+                    break;
+                case 6:
+                    Console.WriteLine(" #### \n#     \n #### \n#   # \n #### ");
+                    break;
+                case 7:
+                    Console.WriteLine(" #### \n     #\n     #\n     #\n     # ");
+                    break;
+                case 8:
+                    Console.WriteLine(" #### \n#   # \n #### \n#   # \n #### ");
+                    break;
+                case 9:
+                    Console.WriteLine(" #### \n#   # \n #### \n     #\n #### ");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // 1. Вывести на экран массив из 20 случайных чисел. Ввести два числа из этого массива, которые нужно поменять местами. Вывести на экран получившийся массив.
+            Console.WriteLine("1. Вывести на экран массив из 20 случайных чисел. Ввести два числа из этого массива,которые нужно поменять местами. Вывести на экран получившийся массив.");
+            Random random = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(1, 1001);
+            Console.WriteLine("Исходный массив:");
+            Console.WriteLine(string.Join(", ", array));
+            Console.Write("Введите первое число для обмена: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число для обмена: ");
+            int number2 = int.Parse(Console.ReadLine());
+            int ind1 = Array.IndexOf(array, number1);
+            int ind2 = Array.IndexOf(array, number2);
+            if (ind1 == -1 || ind2 == -1)
+            {
+                Console.WriteLine("Числа не найдены в массиве.");
+            }
+            else
+            {
+                (array[ind1], array[ind2]) = (array[ind2], array[ind1]);
+
+            }
+            Console.WriteLine($"Массив после обмена: {string.Join(", ", array)}");
+            // 2. Написать метод, где в качества аргумента будет передан массив (ключевое слово params). Вывести сумму элементов массива(вернуть). Вывести(ref) произведение массива.Вывести(out) среднее арифметическое в массиве.
+            Console.WriteLine("2. Написать метод, где в качества аргумента будет передан массив (ключевое слово params). Вывести сумму элементов массива (вернуть). Вывести (ref) произведение массива. Вывести (out) среднее арифметическое в массиве.");
+            long proizv = 1;
+            double sum = Calculation_Array(out double sr_arif, ref proizv, 1, 2, 3, 4, 5);
+            Console.WriteLine($"Сумма элементов: {sum}");
+            Console.WriteLine($"Произведение элементов: {proizv}");
+            Console.WriteLine($"Среднее арифметическое: {sr_arif}");
+            //3. Пользователь вводит числа. Если числа от 0 до 9, то необходимо в консоли нарисовать изображение цифры в виде символа #.Если число больше 9 или меньше 0, то консоль должна окраситься в красный цвет на 3 секунды и вывести сообщение об ошибке. Если пользователь ввёл не цифру, то программа должна выпасть в исключение. Программа завершает работу, если пользователь введёт слово: exit или закрыть(оба варианта должны сработать) - консоль закроется.
+            Console.WriteLine("3. Пользователь вводит числа. Если числа от 0 до 9, то необходимо в консоли нарисовать изображение цифры в виде символа #.Если число больше 9 или меньше 0, то консоль должна окраситься в красный цвет на 3 секунды и вывести сообщение об ошибке. Если пользователь ввёл не цифру, то программа должна выпасть в исключение. Программа завершает работу, если пользователь введёт слово: exit или закрыть (оба варианта должны сработать) - консоль закроется.");
+            while (true)
+            {
+                Console.Write("Введите цифру от 0 до 9 или 'exit'/'закрыть' для выхода: ");
+                string num = Console.ReadLine();
+                if ((num.ToLower() == "exit") || num.ToLower() == "закрыть")
+                {
+                    break;
+                }
+                if (int.TryParse(num, out int digit) && digit >= 0 && digit <= 9)
+                {
+                    Draw_Digit(digit);
+                }
+                else
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка! Введите цифру от 0 до 9.");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.ResetColor();
+                }  
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tumakov_Labs/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Tumakov_Labs
+{
+    internal class Program
+    {
+        static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+        static void Exchange(ref int a, ref int b)
+        {
+            int kopi = a;
+            a = b;
+            b = kopi;
+        }
+        static bool Factorial(int n, out long result)
+        {
+            result = 1;
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        result *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+        }
+        static long Rec_factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * Rec_factorial(n - 1);
+        }
+        static int NOD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        static int NOD(int a, int b, int c)
+        {
+            return NOD(NOD(a, b), c);
+        }
+        static long Fibonacci(int n)
+        {
+            if (n <= 1) return n;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            //Упражнение 5.1 Написать метод, возвращающий наибольшее из двух чисел. Входные параметры метода – два целых числа.Протестировать метод.
+            Console.WriteLine("Упражнение 5.1 Написать метод, возвращающий наибольшее из двух чисел. Входные параметры метода – два целых числа. Протестировать метод.");
+            Console.Write("Введите первое число: ");
+            int n11 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int n12 = int.Parse(Console.ReadLine());
+            int max_n = Max(n11, n12);
+            Console.WriteLine($"Наибольшее число: {max_n}");
+            // Упражнение 5.2 Написать метод, который меняет местами значения двух передаваемых параметров.Параметры передавать по ссылке. Протестировать метод.
+            Console.WriteLine("Упражнение 5.2 Написать метод, который меняет местами значения двух передаваемых параметров. Параметры передавать по ссылке. Протестировать метод.");
+            Console.Write("Введите первое число: ");
+            int n21 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int n22 = int.Parse(Console.ReadLine());
+            Exchange(ref n21, ref n22);
+            Console.WriteLine($"После обмена: первое число = {n21}, второе число = {n22}");
+            // Упражнение 5.3 Написать метод вычисления факториала числа, результат вычислений передавать в выходном параметре. Если метод отработал успешно, то вернуть значение true; если в процессе вычисления возникло переполнение, то вернуть значение false.Для отслеживания переполнения значения использовать блок checked.
+            Console.WriteLine("Упражнение 5.3 Написать метод вычисления факториала числа, результат вычислений передавать в выходном параметре. Если метод отработал успешно, то вернуть значение true; если в процессе вычисления возникло переполнение, то вернуть значение false.Для отслеживания переполнения значения использовать блок checked.");
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            if (Factorial(number, out long factorial))
+                Console.WriteLine($"{number}! = {factorial}");
+            else
+                Console.WriteLine("Произошло переполнение при вычислении факториала.");
+            // Упражнение 5.4 Написать рекурсивный метод вычисления факториала числа.
+            Console.WriteLine("Упражнение 5.4 Написать рекурсивный метод вычисления факториала числа.");
+            Console.Write("Введите число для вычисления факториала: ");
+            int num = int.Parse(Console.ReadLine());
+            long rec_factorial = Rec_factorial(num);
+            Console.WriteLine($"Факториал {num} = {rec_factorial}");
+            //Домашнее задание 5.1 Написать метод, который вычисляет НОД двух натуральных чисел (алгоритм Евклида). Написать метод с тем же именем, который вычисляет НОД трех натуральных чисел.
+            Console.WriteLine("Домашнее задание 5.1 Написать метод, который вычисляет НОД двух натуральных чисел (алгоритм Евклида). Написать метод с тем же именем, который вычисляет НОД трех натуральных чисел.");
+            Console.Write("Введите первое число: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"НОД двух чисел: {NOD(num1, num2)}");
+            Console.WriteLine($"НОД трех чисел: {NOD(num1, num2, num3)}");
+            // Домашнее задание 5.2 Написать рекурсивный метод, вычисляющий значение n-го числа ряда Фибоначчи. Ряд Фибоначчи – последовательность натуральных чисел 1, 1, 2, 3, 5, 8, 13... Для таких чисел верно соотношение Fk = Fk - 1 + Fk - 2.
+            Console.WriteLine("Домашнее задание 5.2 Написать рекурсивный метод, вычисляющий значение n-го числа ряда Фибоначчи.");
+            Console.Write("Введите номер числа Фибоначчи: ");
+            int n = int.Parse(Console.ReadLine());
+            long num_fib = Fibonacci(n);
+            Console.WriteLine($"{n} число ряда Фибоначчи = {num_fib}");
+            Console.ReadKey();
+        }
+    }
+}
